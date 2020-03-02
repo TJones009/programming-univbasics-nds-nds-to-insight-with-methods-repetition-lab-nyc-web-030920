edcc350553cea909f54b5c7ed2542187791d0f81
Trevor Jones <tjones009@yahoo.com>
2020-03-02 17:23:56 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -57,7 +57,7 @@ def total_gross(source)
   while constant < dir_name.length do
     dir_name = dir_names(constant)
     total += dir_hash[dir_name]
-    constant += 1
+    constat += 1
 end
 
   total
